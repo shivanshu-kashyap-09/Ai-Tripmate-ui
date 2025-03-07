41973f69958faaa0ab6b0f26607e6bec5ee52e24
unknown <kashyapshivanshu63@gmail.com>
2025-03-08 00:52:09 +0530

    change

--- Ai-tripmate-ui/src/Pages/HomePage.jsx
+++ Ai-tripmate-ui/src/Pages/HomePage.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import Header from '../common-components/Header'
 import Hero from '../common-components/Hero'
-import ServiceCard from '../cards/ServiceCard'
 import Recommended from '../common-components/Recommended'
 import About from '../common-components/About'
 import Feedback from '../common-components/Feedback'
-import Footer from '../common-components/Footer'
-import logo2 from "../assets/logo.jpg";
-import TravelCard from '../cards/TravelCard'
-import TripCard from '../cards/TripCard'
-import FullTripCard from '../cards/FullTripCard'
 import ServicesCards from "../cards/ServicesCards";
 
 function HomePage() {
