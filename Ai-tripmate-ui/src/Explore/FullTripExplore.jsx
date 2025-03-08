01e4382ad87e2a7474b8a70827ad8a618cc2ad43
Ashish-gurung-10 <ashishgurung024@gmail.com>
2025-03-08 11:53:19 +0530

    change2a

--- Ai-tripmate-ui/src/Explore/FullTripExplore.jsx
+++ Ai-tripmate-ui/src/Explore/FullTripExplore.jsx
@@ -85,6 +85,7 @@ const FullTripExplore = () => {
             <ServiceDetailCard/>
             <ServiceDetailCard/>
             <ServiceDetailCard/>
+            <ServiceDetailCard/>
             </>
     );
 };
